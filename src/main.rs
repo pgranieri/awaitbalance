8fda375fd16ef2f2c1e9ef35998a06b3af615923
Patrick Granieri <pgranieri@gmail.com>
2025-06-25 15:34:26 -0700

    feat: enable 3 leds blinking asynchronously
    Problem: previously only one LED would blink
    
    Solution: now it do 3
    
    Testing: ran locally
    
    Issue: n/a

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
 use embassy_stm32::Config;
 use embassy_stm32::time::Hertz;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let mut config = Config::default();
 
     {
         use embassy_stm32::rcc::*;
 
-        // By default, HSE on the board comes from a 8 MHz clock signal (not a crystal) from STLink
+        // By default, HSE on the board comes from an 8 MHz clock signal (not a crystal) from STLink
         config.rcc.hse = Some(Hse {
             freq: Hertz(8_000_000),
             mode: HseMode::Bypass,
@@ -48,15 +48,22 @@ async fn main(_spawner: Spawner) {
 
     info!("Hello, World!");
 
-    let mut led = Output::new(p.PB14, Level::High, Speed::Low);
+    spawner.spawn(led_task(p.PB0.degrade(), 1000, "Green")).unwrap();
+    spawner.spawn(led_task(p.PB7.degrade(), 1010, "Blue")).unwrap();
+    spawner.spawn(led_task(p.PB14.degrade(), 1020, "Red")).unwrap();
+}
+
+#[embassy_executor::task(pool_size = 3)]
+async fn led_task(pin: AnyPin, delay: u64, name: &'static str) {
+    let mut led = Output::new(pin, Level::High, Speed::Low);
 
     loop {
-        info!("LED High");
         led.set_high();
-        Timer::after_millis(1000).await;
+        info!("{}: LED High", name);
+        Timer::after_millis(delay).await;
 
-        info!("LED Low");
         led.set_low();
-        Timer::after_millis(1000).await;
+        info!("{}: LED Low", name);
+        Timer::after_millis(delay).await;
     }
 }
