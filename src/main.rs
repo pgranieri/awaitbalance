00a0919fd95e662c66f60f28792e2bd3d491b61f
Patrick Granieri <pgranieri@gmail.com>
2025-07-05 22:22:20 -0700

    feat: initialize peripherals for BNO085
    Problem: Missing peripherals to interface with the BNO085 peripheral
    
    Solution: Create a new task to handle SPI communication. Set pins, DMA
    streams and EXTI channel for SPI interface with BNO085. Reset the IMU by
    driving the RST pin, then wait for the interrupt after startup.
    
    Testing: Ran locally and received following output:
    ```
    0.000030 [INFO ] Hello, World!
    0.000488 [INFO ] Red: LED High
    0.000915 [INFO ] Blue: LED High
    0.001342 [INFO ] Green: LED High
    0.001739 [INFO ] SPI1 task begin
    0.002349 [INFO ] Reset BNO085, waiting for interrupt...
    0.097167 [INFO ] Received BNO085 interrupt!
    1.001770 [INFO ] Green: LED Low
    1.011322 [INFO ] Blue: LED Low
    1.020904 [INFO ] Red: LED Low
    2.002227 [INFO ] Green: LED High
    2.021759 [INFO ] Blue: LED High
    2.041320 [INFO ] Red: LED High
    3.002685 [INFO ] Green: LED Low
    3.032196 [INFO ] Blue: LED Low
    3.061737 [INFO ] Red: LED Low
    4.003143 [INFO ] Green: LED High
    4.042633 [INFO ] Blue: LED High
    4.082153 [INFO ] Red: LED High
    5.003601 [INFO ] Green: LED Low
    5.053070 [INFO ] Blue: LED Low
    5.097442 [INFO ] [SPI] It's lonely here...
    5.102569 [INFO ] Red: LED Low
    ```
    
    Issue: n/a

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,58 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
-use embassy_stm32::Config;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Pull, Speed};
+use embassy_stm32::peripherals::{DMA2_CH2, DMA2_CH3, EXTI6, PA5, PA6, PB15, PB5, PC6, PC7, SPI1};
+use embassy_stm32::spi::Spi;
+use embassy_stm32::{spi, Config};
 use embassy_stm32::time::Hertz;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    /*
+        Pin Map
+        CS:     PC_7
+        SCK:    PB_3
+        MISO:   PB_4
+        MOSI:   PB_5
+        INT:    PC_6
+        RST:    PB_15
+     */
+
+    let p = embassy_stm32::init(set_clock_config());
+
+    info!("Hello, World!");
+
+    // Clock source for SPI1 is APB2, which is set to 60MHz
+    // BNO085 has a maximum SPI clock speed of 3MHz
+    // Set SPI clock divider to 32, giving a baud rate of 1.875MHz
+    let mut spi_config = spi::Config::default();
+    spi_config.frequency = Hertz(1_875_000);
+
+    spawner.spawn(
+        spi1_task(
+            p.SPI1,
+            p.PA5,
+            p.PB5,
+            p.PA6,
+            p.DMA2_CH3,
+            p.DMA2_CH2,
+            p.PC7,
+            p.PC6,
+            p.EXTI6,
+            p.PB15,
+            spi_config
+        )
+    ).unwrap();
+    spawner.spawn(led_task(p.PB0.degrade(), 1000, "Green")).unwrap();
+    spawner.spawn(led_task(p.PB7.degrade(), 1010, "Blue")).unwrap();
+    spawner.spawn(led_task(p.PB14.degrade(), 1020, "Red")).unwrap();
+}
+
+fn set_clock_config() -> Config {
     let mut config = Config::default();
 
     {
@@ -44,13 +88,7 @@ async fn main(spawner: Spawner) {
         config.rcc.apb2_pre = APBPrescaler::DIV2;
     }
 
-    let p = embassy_stm32::init(config);
-
-    info!("Hello, World!");
-
-    spawner.spawn(led_task(p.PB0.degrade(), 1000, "Green")).unwrap();
-    spawner.spawn(led_task(p.PB7.degrade(), 1010, "Blue")).unwrap();
-    spawner.spawn(led_task(p.PB14.degrade(), 1020, "Red")).unwrap();
+    config
 }
 
 #[embassy_executor::task(pool_size = 3)]
@@ -65,5 +103,44 @@ async fn led_task(pin: AnyPin, delay: u64, name: &'static str) {
         led.set_low();
         info!("{}: LED Low", name);
         Timer::after_millis(delay).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn spi1_task(
+    spi1: SPI1,
+    sck_pin: PA5,
+    mosi_pin: PB5,
+    miso_pin: PA6,
+    tx_dma: DMA2_CH3,
+    rx_dma: DMA2_CH2,
+    cs_pin: PC7,
+    int_pin: PC6,
+    exti6: EXTI6,
+    rst_pin: PB15,
+    spi_config: spi::Config,
+) {
+    info!("SPI1 task begin");
+
+    let mut _spi = Spi::new(spi1, sck_pin, mosi_pin, miso_pin, tx_dma, rx_dma, spi_config);
+    let mut spi_int = ExtiInput::new(int_pin, exti6, Pull::None);
+    let mut _cs = Output::new(cs_pin, Level::High, Speed::Low);
+    let mut rst = Output::new(rst_pin, Level::High, Speed::Low);
+
+    /* drive the RST pin low to reset the BNO085 */
+    rst.set_low();
+    Timer::after_ticks(1).await; // 10ns minimum hold time, tick period is ~30.5us
+    rst.set_high();
+
+    info!("Reset BNO085, waiting for interrupt...");
+
+    /* wait for BNO085 to finish resetting */
+    spi_int.wait_for_falling_edge().await; // 90ms min for response
+
+    info!("Received BNO085 interrupt!");
+
+    loop {
+        Timer::after_secs(5).await;
+        info!("[SPI] It's lonely here...");
     }
 }
